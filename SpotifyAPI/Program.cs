f9d27da1a151611bfcf25a5f2316f50e54bc6bf7
1017251 <1017251@CRN-323E-13-D>
2023-10-26 13:58:39 -0400

    Add project files.

---
+++ SpotifyAPI/Program.cs
@@ -0,0 +1,31 @@
+﻿using SpotifyAPI.Web;
+
+
+namespace SpotifyAPI
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SpotifyClient client = new SpotifyClient(YourAPIKey.API_KEY);
+
+            GetTrack(client, (v) =>
+            {
+                Console.WriteLine(v.Album);
+                Console.WriteLine(v.Artists);
+                Console.WriteLine(v.Explicit ? "This song is explicit" : "This song is not explicit");
+            });
+        }
+
+        public static async void GetTrack(SpotifyClient client, Action<FullTrack> OnReturn)
+        {
+            FullTrack track = await client.Tracks.Get("put the song ID here");
+            OnReturn(track);
+        }
+
+        internal static class YourAPIKey
+        {
+            public const string API_KEY = "blah blah blah get a key from here: https://developer.spotify.com/documentation/web-api";
+        }
+    }
+}
